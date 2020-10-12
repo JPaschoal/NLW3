7430221fcb17c12057ca48b3039d9be9e749ed1a
Jhonatan Paschoal <jhonatan.paschoal@acct.global>
2020-10-12 13:53:52 -0300

    creating OrphanagesMap

---
+++ src/pages/OrphanegesMap.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function OrphanagesMap() {
+  return (
+    <h1>OrphanagesMap</h1>
+  );
+}
+
+export default OrphanagesMap;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Landing from './pages/Landing'
+import OrphanegesMap from './pages/OrphanegesMap'
 
 function Routes() {
   return(
     <BrowserRouter>
-      <Route path='/' component={Landing} />
+      <Switch>
+        <Route path='/' exact component={Landing} />
+        <Route path='/app' component={OrphanegesMap}/>
+      </Switch>
     </BrowserRouter>
   );
 }
